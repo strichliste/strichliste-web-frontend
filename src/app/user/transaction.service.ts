7e4b3c7dbe0cbfda244ee7aeaea26904155c2bec
sander <funkpunkdrummer@gmail.com>
2016-11-07 22:25:51 +0100

    added get user transactions

--- src/app/user/transaction.service.ts
+++ src/app/user/transaction.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http, URLSearchParams} from '@angular/http';
 import {TallyConfig} from '../shared/tally.config';
 
 @Injectable()
 export class TransactionService {
 
-  constructor(private http:Http) { }
+  constructor(private http: Http) {
+  }
 
   addTransaction(userId, value) {
     return this.http.post(`${TallyConfig.URL}user/${userId}/transaction`, {value}).map(res => res.json());
   }
 
+  getTransactions(userId, limit, offset) {
+    let params = new URLSearchParams();
+    params.set('limit', limit);
+    params.set('offset', offset);
+
+    return this.http.get(`${TallyConfig.URL}user/${userId}/transaction`, {search: params})
+      .map(res => res.json());
+  }
 }
--- src/app/user/user-transactions/user-transactions.component.ts
+++ src/app/user/user-transactions/user-transactions.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {TransactionService} from '../transaction.service';
+import {AlertsService} from '../../shared/alerts/alerts.service';
+import {AlertModel} from '../../shared/alerts/alert.model';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'tally-user-transactions',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-transactions.component.less']
 })
 export class UserTransactionsComponent implements OnInit {
-
-  constructor() { }
+  userId:number;
+  transactions: any[];
+  queryParamsSubscribtion:any;
+  constructor(private transactionService: TransactionService,
+              private route:ActivatedRoute,
+              private alertsService: AlertsService) {
+    this.queryParamsSubscribtion = this.route.params.subscribe((param) => {
+      this.userId = param['id'];
+    });
+  }
 
   ngOnInit() {
+    this.getUserTransactions(10, 0);
+  }
+
+  getUserTransactions(limit, offset) {
+    this.transactionService.getTransactions(this.userId, limit, offset).toPromise().then((res: any) => {
+      if (res && res.entries) {
+        this.transactions = res.entries;
+      }
+    }, (err) => {
+      this.alertsService.add(new AlertModel('danger', err));
+    }) ;
   }
 
 }
