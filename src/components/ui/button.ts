f256ec06dfc0ba5710d92b0df7d09935369e4f16
Tobias Pickel <tobias.pickel@xing.com>
2018-08-18 23:58:50 +0200

    Refactor button to use ripple design

--- src/components/ui/button.ts
+++ src/components/ui/button.ts
@@ -5,12 +5,30 @@ export interface ButtonProps {
   color?: string;
   disabled?: boolean;
   type?: string;
+  margin?: string;
 }
 
 export const Button = styled('button')<ButtonProps>(
   {
-    margin: '0.5rem',
-    padding: '0.5rem',
+    border: 'none',
+    borderRadius: '2px',
+    padding: ' 0.5rem',
+    cursor: 'pointer',
+    color: 'white',
+    backgroundColor: '#2196f3',
+    boxShadow: '0 0 4px #999',
+    outline: 'none',
+    backgroundPosition: 'center',
+    transition: 'background 0.8s',
+    '&:hover': {
+      background:
+        '#47a7f5 radial - gradient(circle, transparent 1 %, #47a7f5 1 %) center / 15000 %',
+    },
+    '&:active': {
+      backgroundColor: '#fff',
+      backgroundSize: '100%',
+      transition: 'background 0s',
+    },
   },
   props => ({
     background: props.color,
