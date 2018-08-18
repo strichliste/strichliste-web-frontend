3e370340fd37d48b375aa505aeb24d4612d32a0f
Tobias Pickel <tobias.pickel@xing.com>
2018-08-18 19:49:44 +0200

    Add locale strings

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,3 +1,8 @@
 export const en = {
   TALLY_HEADER: 'Tally',
+  USER_ACTIVE: 'Active user',
+  USER_INACTIVE: 'Inactive user',
+  USER_CREATE_HEADLINE: 'Create new user',
+  USER_CREATE_TRIGGER: 'Create user',
+  USER_CREATE_NAME_LABEL: 'Name',
 };
