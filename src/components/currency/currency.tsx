11d04e19fe5524fba5e956bad4310c4cefede79e
Tobias Pickel <tobias.pickel@xing.com>
2018-08-18 19:46:43 +0200

    Add currency component
    todo connect currency component

---
+++ src/components/currency/currency.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { FormattedNumber } from 'react-intl';
+import { connect } from 'react-redux';
+
+interface OwnProps {
+  value: number;
+}
+
+// interface StateProps
+
+type Props = OwnProps;
+
+export function Currency(props: Props): JSX.Element {
+  return (
+    <FormattedNumber
+      currency="EUR"
+      value={props.value / 100}
+      style="currency"
+    />
+  );
+}
+
+export const ConnectedCurrency = connect()(Currency);
