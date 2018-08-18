634bc6df489c4a8c05fd3a3018d6c2e1cb33812f
Tobias Pickel <tobias.pickel@xing.com>
2018-08-19 00:27:05 +0200

    Updated locals

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -2,6 +2,7 @@ export const en = {
   TALLY_HEADER: 'Tally',
   USER_ACTIVE: 'Active user',
   USER_INACTIVE: 'Inactive user',
+  USER_TRANSACTIONS: 'Transactions',
   USER_CREATE_HEADLINE: 'Create new user',
   USER_CREATE_TRIGGER: 'Create user',
   USER_CREATE_NAME_LABEL: 'Name',
