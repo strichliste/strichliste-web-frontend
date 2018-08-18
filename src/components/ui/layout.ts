6a04a89502eb43dee3ad6c709a360f380a3cf6e8
Tobias Pickel <tobias.pickel@xing.com>
2018-08-18 19:47:40 +0200

    Add layout components

---
+++ src/components/ui/layout.ts
@@ -0,0 +1,19 @@
+import styled from 'react-emotion';
+
+export const FormField = styled('div')({
+  marginBottom: '0.5rem',
+});
+
+export const Section = styled('section')({
+  padding: '1.5rem',
+});
+
+export const Flex = styled('div')({
+  display: 'flex',
+});
+
+export const CenterSection = styled(Section)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignContent: 'center',
+});
