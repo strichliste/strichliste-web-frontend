f0c03b3981ce8576602f23a1569668ae6f7d2108
Tobias Pickel <tobias.pickel@xing.com>
2018-08-18 19:47:32 +0200

    Add dynamic props to card

--- src/components/ui/card.ts
+++ src/components/ui/card.ts
@@ -1,21 +1,35 @@
 import styled from 'react-emotion';
 
-export const Card = styled('div')({
-  fontSize: '1rem',
-  padding: '1rem',
-  margin: '1rem',
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  position: 'relative',
-  width: '10rem',
-  height: '10rem',
-  borderRadius: '2px',
-  background: '#fff',
-  boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
-  transition: 'all 0.3s cubic-bezier(.25, .8, .25, 1)',
-  '&:hover': '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);',
-});
+interface CardProps {
+  hover?: boolean;
+  width?: string;
+  height?: string;
+}
+
+export const Card = styled('div')<CardProps>(
+  {
+    fontSize: '1rem',
+    padding: '1rem',
+    margin: '1rem',
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    borderRadius: '2px',
+    background: '#fff',
+    boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+    transition: 'all 0.3s cubic-bezier(.25, .8, .25, 1)',
+  },
+  props => ({
+    width: props.width,
+    height: props.height,
+    '&:hover': {
+      boxShadow: props.hover
+        ? '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);'
+        : '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+    },
+  })
+);
 
 export const CardContainer = styled('div')({
   width: '100%',
