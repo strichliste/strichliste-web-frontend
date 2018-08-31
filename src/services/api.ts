df515eaa807a84860adb30673b2ee7b8e28497c9
Tobias Pickel <tobias.pickel@xing.com>
2018-08-31 16:33:48 +0200

    feat(api): add production and devolpement api urls

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
-const API_URL = 'http://v2.strichliste.org/api/';
+const API_URL = process.env.REACT_APP_API;
 
 export async function fetchJson(
   endpoint: string,
   options: RequestInit = {}
   // tslint:disable-next-line:no-any
 ): Promise<any> {
-  return fetch(API_URL + endpoint, options).then(async res => res.json());
+  return fetch(`${API_URL}${endpoint}`, options).then(async res => res.json());
 }
 
 // tslint:disable-next-line:no-any
@@ -16,10 +16,10 @@ export async function post(endpoint: string, params: any): Promise<any> {
     body: JSON.stringify(params),
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
   };
-  return fetch(API_URL + endpoint, options).then(async res => res.json());
+  return fetch(`${API_URL}${endpoint}`, options).then(async res => res.json());
 }
 
 // tslint:disable-next-line:no-any
 export async function get(endpoint: string): Promise<any> {
-  return fetch(API_URL + endpoint).then(async res => res.json());
+  return fetch(`${API_URL}${endpoint}`).then(async res => res.json());
 }
