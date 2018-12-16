4f9486cbd9fd826f603142198c543fcb9453486d
sanderdrummer <funkpunkdrummer@gmail.com>
2018-12-16 19:57:43 +0100

    test(pager): update button selector

--- src/components/common/__tests__/pager.spec.tsx
+++ src/components/common/__tests__/pager.spec.tsx
@@ -11,8 +11,8 @@ describe('Pager', () => {
     const { getByText } = renderWithIntl(
       <Pager currentPage={0} limit={5} itemCount={4} onChange={jest.fn()} />
     );
-    const prev = getByText('PAGER_PREV');
-    const next = getByText('PAGER_NEXT');
+    const prev = getByText('PAGER_PREV', { exact: false });
+    const next = getByText('PAGER_NEXT', { exact: false });
     expect(prev.hasAttribute('disabled')).toBeTruthy();
     expect(next.hasAttribute('disabled')).toBeTruthy();
   });
@@ -21,8 +21,8 @@ describe('Pager', () => {
     const { getByText } = renderWithIntl(
       <Pager currentPage={0} limit={5} itemCount={10} onChange={jest.fn()} />
     );
-    const prev = getByText('PAGER_PREV');
-    const next = getByText('PAGER_NEXT');
+    const prev = getByText('PAGER_PREV', { exact: false });
+    const next = getByText('PAGER_NEXT', { exact: false });
     expect(prev.hasAttribute('disabled')).toBeTruthy();
     expect(next.hasAttribute('disabled')).toBeFalsy();
   });
@@ -31,8 +31,8 @@ describe('Pager', () => {
     const { getByText } = renderWithIntl(
       <Pager currentPage={2} limit={5} itemCount={40} onChange={jest.fn()} />
     );
-    const prev = getByText('PAGER_PREV');
-    const next = getByText('PAGER_NEXT');
+    const prev = getByText('PAGER_PREV', { exact: false });
+    const next = getByText('PAGER_NEXT', { exact: false });
     expect(prev.hasAttribute('disabled')).toBeFalsy();
     expect(next.hasAttribute('disabled')).toBeFalsy();
   });
@@ -45,13 +45,13 @@ describe('Pager', () => {
       );
     });
     it('goes to next page on next click', () => {
-      const next = result.getByText('PAGER_NEXT');
+      const next = result.getByText('PAGER_NEXT', { exact: false });
 
       fireEvent.click(next);
       expect(onChange).toBeCalledWith(3);
     });
     it('goes to prev page on prev click', () => {
-      const prev = result.getByText('PAGER_PREV');
+      const prev = result.getByText('PAGER_PREV', { exact: false });
 
       fireEvent.click(prev);
       expect(onChange).toBeCalledWith(1);
