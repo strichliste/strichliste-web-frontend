f24416bfe6a2ac2847bdec425d7ce815a2795ddf
Tobias Pickel <tobias.pickel@xing.com>
2019-03-05 18:13:50 +0100

    fix(scaling-buttons): update design

--- src/components/settings/scaling-buttons.tsx
+++ src/components/settings/scaling-buttons.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocalStorage } from '../../hooks/use-storage';
-import { Flex, Button, styled } from 'bricks-of-sand';
+import { TextButton, SearchMinus, SearchPlus, Icon } from 'bricks-of-sand';
 
-const Wrapper = styled(Flex)({
-  fontSize: '0.7rem',
-});
 export const useScalingState = () => {
   const [scaling, setScaling] = useLocalStorage('scaling', 0);
   const increment = () => {
@@ -23,22 +20,34 @@ export const useScalingState = () => {
   };
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let id: any = 0;
 export const ScalingButtons = () => {
   const { increment, decrement, scaling } = useScalingState();
+  const [isVisible, setIsVisible] = React.useState(true);
+
+  useEffect(() => {
+    id = setTimeout(() => {
+      setIsVisible(false);
+    }, 4000);
+    return () => clearTimeout(id);
+  }, []);
+
   return (
-    <Wrapper
-      padding="0.5rem"
-      alignContent="center"
-      alignItems="center"
-      justifyContent="flex-end"
-    >
-      <Button margin="0 1rem" onClick={decrement}>
-        -
-      </Button>
-      {scaling}
-      <Button margin="0 1rem" onClick={increment}>
-        +
-      </Button>
-    </Wrapper>
+    <>
+      <TextButton onClick={decrement}>
+        <Icon width="1rem" height="1rem">
+          <SearchMinus />
+        </Icon>
+      </TextButton>
+
+      {isVisible && <TextButton>{scaling}</TextButton>}
+
+      <TextButton onClick={increment}>
+        <Icon width="1rem" height="1rem">
+          <SearchPlus />
+        </Icon>
+      </TextButton>
+    </>
   );
 };
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,44 +1,38 @@
 import { Footer } from 'bricks-of-sand';
 import * as React from 'react';
 import { GitHubIcon } from '../ui/icons/git-hub';
-import { ScalingButtons } from '../settings/scaling-buttons';
 
 export function MainFooter(): JSX.Element {
   return (
-    <>
-      <Footer>
-        <div>
-          strichliste-web (MIT License, by{' '}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/schinken"
-          >
-            schinken
-          </a>{' '}
-          and{' '}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/sanderdrummer"
-          >
-            sanderdrummer
-          </a>
-          )
-        </div>
-        <div>
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/strichliste"
-          >
-            <GitHubIcon /> Code
-          </a>
-        </div>
-      </Footer>
+    <Footer>
       <div>
-        <ScalingButtons />
+        strichliste-web (MIT License, by{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/schinken"
+        >
+          schinken
+        </a>{' '}
+        and{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/sanderdrummer"
+        >
+          sanderdrummer
+        </a>
+        )
       </div>
-    </>
+      <div>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/strichliste"
+        >
+          <GitHubIcon /> Code
+        </a>
+      </div>
+    </Footer>
   );
 }
