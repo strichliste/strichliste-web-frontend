67c98aaf6e802eb6e1cc5ec8341dace10c99cd06
Tobias Pickel <tobias.pickel@xing.com>
2019-06-17 13:16:49 +0200

    fix(metrics): updated api

--- src/components/metrics/resource.ts
+++ src/components/metrics/resource.ts
@@ -3,23 +3,36 @@ import { useState } from 'react';
 import { get, useEffectAsync } from '../../services/api';
 
 export const useMetrics = (): Metric | null => {
-  const [metric, setMetrics] = useState(null);
+  const [metric, setMetrics] = useState<Metric | null>(null);
 
   useEffectAsync(async () => {
-    const nextMetrics = await get(`metrics`);
-    setMetrics(nextMetrics);
+    const nextMetrics: Metric = await get(`metrics`);
+
+    const formattedMetric = {
+      ...nextMetrics,
+      days: nextMetrics.days.map(day => ({
+        balance: day.balance / 100,
+        charged: day.charged / 100,
+        date: day.date,
+        distinctUsers: day.distinctUsers,
+        spent: day.spent / 100,
+        transactions: day.transactions,
+      })),
+    };
+
+    setMetrics(formattedMetric);
   }, []);
 
   return metric;
 };
 
 export interface Day {
-  date: string;
-  count: string;
-  distinctUsers: string;
   balance: number;
-  positiveBalance: number;
-  negativeBalance: number;
+  charged: number;
+  date: string;
+  distinctUsers: number;
+  spent: number;
+  transactions: number;
 }
 
 export interface Metric {
