190a664a28ac242db9887678f8935c61d09e97d4
Tobias Pickel <tobias.pickel@xing.com>
2019-07-08 16:50:08 +0200

    feat(user-router): add useUserDetailUrl

--- src/components/common/search-results/search-results.tsx
+++ src/components/common/search-results/search-results.tsx
@@ -5,10 +5,12 @@ import { useUserArray } from '../../../store';
 import { SearchList } from '../search-list/search-list';
 import { SearchResultItem } from './search-result-item/search-result-item';
 import { RouteComponentProps } from 'react-router';
+import { useUserDetailUrl } from '../../user/user-router';
 
 export const SearchResults: React.FC<RouteComponentProps> = props => {
   const userArray = useUserArray();
   const dispatch = useDispatch();
+  const userDetailUrl = useUserDetailUrl();
   useEffect(() => {
     startLoadingUsers(dispatch);
   }, [dispatch]);
@@ -21,7 +23,7 @@ export const SearchResults: React.FC<RouteComponentProps> = props => {
           <SearchResultItem
             key={user.id}
             name={user.name}
-            onClick={() => console.log(user)}
+            onClick={() => props.history.push(userDetailUrl(user.id))}
           />
         )}
         items={userArray}
