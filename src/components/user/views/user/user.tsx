51a34cd0c4b9e32eedae2871641c167a2c63a872
Tobias Pickel <tobias.pickel@xing.com>
2019-10-07 11:47:12 +0200

    refactor(user): migrate user from emotion

--- src/components/user/views/user/user.tsx
+++ src/components/user/views/user/user.tsx
@@ -1,15 +1,16 @@
-import { breakPoints, styled } from 'bricks-of-sand';
 import React, { useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { RouteComponentProps } from 'react-router-dom';
 
-import { useFilteredUsers } from '../../../store';
-import { startLoadingUsers } from '../../../store/reducers';
-import { NavTabMenus } from '../../common/nav-tab-menu';
-import { CreateUserInlineFormView } from '../create-user-inline-form';
+import { useFilteredUsers } from '../../../../store';
+import { startLoadingUsers } from '../../../../store/reducers';
+import { NavTabMenus } from '../../../common/nav-tab-menu';
+import { CreateUserInlineFormView } from '../../create-user-inline-form';
 import { useDispatch } from 'redux-react-hook';
-import { ScrollToTop } from '../../common/scroll-to-top';
-import { UserList } from '../user-list';
+import { ScrollToTop } from '../../../common/scroll-to-top';
+import { UserList } from '../../user-list';
+
+import styles from './user.module.css';
 
 interface OwnProps {
   isActive: boolean;
@@ -17,28 +18,6 @@ interface OwnProps {
 }
 
 type UserProps = OwnProps & RouteComponentProps;
-
-const GridWrapper = styled('div')({
-  marginLeft: '0rem',
-  [breakPoints.tablet]: {
-    marginLeft: '8rem',
-  },
-});
-
-const CreateUserPosition = styled('div')({
-  [breakPoints.tablet]: {
-    zIndex: 10,
-    position: 'absolute',
-    marginLeft: '-2rem',
-    marginTop: '6rem',
-    bottom: 'initial',
-    left: 'initial',
-  },
-  position: 'fixed',
-  bottom: '1rem',
-  left: '1rem',
-  zIndex: 10,
-});
 
 export const User = (props: UserProps) => {
   const userIds = useFilteredUsers(props.isActive);
@@ -51,12 +30,12 @@ export const User = (props: UserProps) => {
   return (
     <>
       <ScrollToTop />
-      <GridWrapper>
-        <CreateUserPosition>
+      <div className={styles.wrapper}>
+        <div className={styles.addUserButton}>
           <CreateUserInlineFormView
             isActive={props.showCreateUserForm || false}
           />
-        </CreateUserPosition>
+        </div>
         <NavTabMenus
           margin="2rem 1rem"
           breakpoint={320}
@@ -73,7 +52,7 @@ export const User = (props: UserProps) => {
           ]}
         />
         <UserList userIds={userIds} />
-      </GridWrapper>
+      </div>
     </>
   );
 };
