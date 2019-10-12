8ecc8662a5c665e9ec7d3da491df1043f8e91a3c
Tobias Pickel <tobias.pickel@xing.com>
2019-10-12 18:58:15 +0200

    feat(article-edit-form-view): add margins

--- src/components/article/article-edit-form-view.tsx
+++ src/components/article/article-edit-form-view.tsx
@@ -1,16 +1,32 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { ArticleForm } from './article-form';
-import { Card } from '../../bricks';
+import { Button } from '../../bricks';
+import { FormattedMessage } from 'react-intl';
 
 export function ArticleEditFormView({
   match,
   history,
 }: RouteComponentProps<{ id: string }>): JSX.Element {
   return (
-    <ArticleForm
-      onCreated={history.goBack}
-      articleId={Number(match.params.id)}
-    />
+    <>
+      <div style={{ margin: '1rem' }}>
+        <Button
+          onClick={() => history.goBack()}
+          children={
+            <>
+              &#8592;
+              <FormattedMessage id="BACK_BUTTON" />
+            </>
+          }
+        />
+      </div>
+      <div style={{ padding: '1rem' }}>
+        <ArticleForm
+          onCreated={history.goBack}
+          articleId={Number(match.params.id)}
+        />
+      </div>
+    </>
   );
 }
