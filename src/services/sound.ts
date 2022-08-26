79497c93151fa775cc36cf4bc45d82e902f3aa4d
David Christofas <dchristofas@posteo.de>
2022-08-26 23:27:25 +0200

    implement random sounds after a transaction
    The default is still only the 'ka-ching' sound. To add more sounds you need
    to place your sound files next to the `ka-ching.wav` file and then add the
    filename to the sounds in the `sounds-config.json` file.
    E.g. (sounds-config.json):
    ```json
    {
        "sounds":["ka-ching.wav", "mario-coin.wav"]
    }
    ```

--- src/services/sound.ts
+++ src/services/sound.ts
@@ -1,11 +1,28 @@
 import { Howl } from 'howler';
 import { CreateTransactionParams } from '../store/reducers';
 
-const dispense = new Howl({
-  src: ['ka-ching.wav'],
-});
+const howls: Howl[] = [];
+fetch('sound-config.json')
+  .then(response => {
+    if (response.status == 404) {
+      throw Error('file "sound-config.json" not found');
+    }
+    return response.json()
+  })
+  .then(config => {
+    const sounds = config['sounds'];
+    for (let i=0; i<sounds.length; i++) {
+      howls.push(new Howl({
+        src: [sounds[i]]
+      }));
+    }
+  })
+  .catch(err => {
+    console.error('Error while handling sound-config', err);
+  });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function playCashSound(_params?: CreateTransactionParams): void {
-  dispense.play();
+  const sound = howls[Math.floor(Math.random() * howls.length)];
+  sound.play();
 }
