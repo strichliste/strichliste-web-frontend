2393bd9e1ef4d621346f3a915040d8f72f087336
Micha Ober <git@ober-mail.de>
2026-05-05 09:35:07 +0200

    Use i18n settings (alpha3, language) from backend for currency rendering (#271)

--- src/components/currency/__tests__/currency.spec.tsx
+++ src/components/currency/__tests__/currency.spec.tsx
@@ -1,33 +1,27 @@
 import * as React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 
-import { IntlProvider } from 'react-intl';
 import { Currency } from '../';
+import { renderWithContext } from '../../../spec-configs/render';
 
 afterEach(cleanup);
 
 describe('Currency', () => {
   it('matches the snapshot for english locales', () => {
-    const { container } = render(
-      <IntlProvider defaultLocale="en">
-        <Currency value={120} />
-      </IntlProvider>
-    );
+    const { container } = renderWithContext(<Currency value={120} />, {});
     expect(container).toMatchSnapshot();
   });
   it('hides plus icons by prop', () => {
-    const { container } = render(
-      <IntlProvider defaultLocale="en">
-        <Currency hidePlusSign value={120} />
-      </IntlProvider>
+    const { container } = renderWithContext(
+      <Currency hidePlusSign value={120} />,
+      {}
     );
     expect(container).toMatchSnapshot();
   });
   it('show - icons', () => {
-    const { container } = render(
-      <IntlProvider defaultLocale="en">
-        <Currency hidePlusSign value={-120} />
-      </IntlProvider>
+    const { container } = renderWithContext(
+      <Currency hidePlusSign value={-120} />,
+      {}
     );
     expect(container).toMatchSnapshot();
   });
